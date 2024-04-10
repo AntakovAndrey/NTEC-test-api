bca556f9bd002a2c8203ea18eca14eedc4814d22
Andrey Antakov <andrey.antakov05@gmail.com>
2024-04-10 11:28:40 +0300

    5

--- sql/001_init.sql
+++ sql/001_init.sql
@@ -4,8 +4,8 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE IF NOT EXISTS PhoneBook (
     id varchar (256) PRIMARY KEY,
     phone varchar (256) NOT NULL,
-    name                   varchar (256) NOT NULL,
-    address                varchar (256) NOT NULL,
+    name varchar (256) NOT NULL,
+    address varchar (256) NOT NULL,
     
 );
 
